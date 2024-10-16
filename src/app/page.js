cfeecd0aa65cde003de00783b3b1cc23536edca8
Shashwat Johri <79015749+notshashwat@users.noreply.github.com>
2024-10-16 21:28:07 +0530

    updated resume and pi42 info

--- src/app/page.js
+++ src/app/page.js
@@ -179,7 +179,7 @@ const projects = [
         <div class="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time class="mb-1 text-sm font-normal leading-none text-black-400 dark:text-pink-500">November 2020 - May 2024</time>
         <h3 class="text-lg font-semibold text-pink-900">B.Tech Computer Science and Engineering, IIT Bhilai</h3>
-        <p class="mb-1 text-base font-normal pink-gray-500 dark:pink-gray-400">CGPA 9.46/10</p>
+        <p class="mb-1 text-base font-normal pink-gray-500 dark:pink-gray-400">CGPA 9.45/10</p>
         <p class="mb-1 text-base font-normal pink-gray-500 dark:pink-gray-400">Courses Taken -  Data Structures, Algorithms, ML, AI, NLP, DBMS, OS, Computer Network, Computer System Design</p>
         <p class="mb-4 text-base font-normal pink-gray-500 dark:pink-gray-400">Volunteer Positions: Coordinator - Quizzing club 2021-22, Core Member - Google Developer Student Club, Member - Literary
 and Debate Society.</p>
@@ -197,12 +197,20 @@ and Debate Society.</p>
         <p class="mb-4 text-base font-normal pink-gray-500 dark:pink-gray-400">Showcased Python proficiency by implementing research papers Reproduced 81% accuracy in road curve classification.</p>
         
     </li>
-    {/* <li class="ms-4">
+     <li class="mb-10 ms-4">
         <div class="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time class="mb-1 text-sm font-normal leading-none text-black-400 dark:text-pink-500">May 2024 - Present</time>
-        <h3 class="text-lg font-semibold text-pink-900">Data Engineer @ Skan</h3>
-        <p class="text-base font-normal text-pink-500 dark:text-pink-400"></p>
-    </li>  */}
+        <time class="mb-1 text-sm font-normal leading-none text-black-400 dark:text-pink-500">June 2024 - present</time>
+        <h3 class="text-lg font-semibold text-pink-900">Backend Engineer @ Pi42 - Crypto Futures Exchange</h3>
+        {/* <p class="text-base font-normal text-pink-500 dark:text-pink-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p> */}
+        <p class="mb-1 text-base font-normal pink-gray-500 dark:pink-gray-400">Spearheaded the end-to-end development of the ”Cross Margin Mode” feature. Utilizing object-oriented programming
+        best practices to ensure a robust and scalable solution in NestJS. </p>
+        <p class="mb-4 text-base font-normal pink-gray-500 dark:pink-gray-400">Implemented caching and pagination within the core APIs, significantly enhancing website performance by optimizing
+        the retrieval and improving overall user experience.</p>
+        <p class="mb-4 text-base font-normal pink-gray-500 dark:pink-gray-400">Designed and implemented a state machine with optimistic locking, effectively resolving concurrency issues and race
+        conditions between multiple services in a complex system.</p>
+        
+    </li>
+   
  </ol>
 
 
